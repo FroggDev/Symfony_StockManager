d839265d71297d5fcce6ddcf7cb860c064866335
Frogg <admin@frogg.fr>
2018-03-28 23:08:25 +0200

    Added public pages & Updated doc

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 # Symfony_StockManager
 Stock Manager based on products barcode
+Using Symfony 4.1
---
+++ public/test/live_w_locator.js
@@ -0,0 +1,48 @@
+$(function() {
+	
+	document.app.Scan.init();
+
+    Quagga.onProcessed(function(result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay,
+            drawingCanvas = Quagga.canvas.dom.overlay;
+
+        if (result) {
+            if (result.boxes) {
+                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                result.boxes.filter(function (box) {
+                    return box !== result.box;
+                }).forEach(function (box) {
+                    Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+                });
+            }
+
+            if (result.box) {
+                Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+            }
+
+            if (result.codeResult && result.codeResult.code) {
+                Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+            }
+        }
+    });
+
+    Quagga.onDetected(function(result) {
+		
+        var code = result.codeResult.code;
+
+        if (document.app.Scan.lastResult !== code) {			
+            
+            var $node = null, canvas = Quagga.canvas.dom.image;
+
+			document.app.Scan.lastResult = code;
+			
+            $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
+            $node.find("img").attr("src", canvas.toDataURL());
+            $node.find("h4.code").html(code);
+            $("#result_strip ul.thumbnails").prepend($node);
+			
+			document.app.Scan.closeScan();
+			
+        }
+    });
+});
